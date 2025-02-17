f19f526bc6e9950b35106f4f710ed67ee056008d
gabpc <grodriguezov@gmail.com>
2025-02-17 00:12:27 -0600

    dashboard initial basic screen added

--- Nexus Parqueo/src/components/Dashboard.jsx
+++ Nexus Parqueo/src/components/Dashboard.jsx
@@ -1,10 +1,13 @@
 import React from 'react';
+import UlacitLogo from '/src/assets/ulacit-logo.png';
+
 
 const Dashboard = () => {
     return (
-        <div className="p-4">
-            <h1>Dashboard</h1>
-            <p>Welcome to the dashboard!</p>
+        <div className="p-5 flex flex-col justify-center items-center">
+            <img src={UlacitLogo} alt="Ulacit Logo"></img>
+            <h1 className="mt-10 font-bold text-5xl">Bienvenido a Parqueos ULACIT</h1>
+            <p className="mt-5 font-semibold text-xl">Usted es (rol)</p>
         </div>
     );
 };
